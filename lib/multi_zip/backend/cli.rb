7a5554cb5135df408f55732666c3d7393484c092
Matthew Nielsen <xunker@pyxidis.org>
2015-04-30 12:13:30 -0600

    WIP scaffold for Cli zipping and unzipping.

---
+++ lib/multi_zip/backend/cli.rb
@@ -0,0 +1,57 @@
+# If no suitable gems are found, this is the last-gasp fallback. We use
+# whatever command-line zipping/unzipping tools we can find and that we
+# know how to use.
+#
+# This will likely be a huge work in progress since it involves a lot of
+# detection. We'll need to detect the host OS and version, the tools available
+# in the path (or specified by the user, optionally) and the command-line
+# arguments to be used for those tools.
+#
+# OS          Unzip                           Zip
+#------------------------------------------------------------------------------
+# OS X 10.10  Info-ZIP UnZip 5.52             Info-ZIP Zip 3.0
+# RHEL-Based  Info-ZIP UnZip 5.xx to 6.xx     Info-ZIP Zip 2.x to 3.x
+# Ubuntu      Info-ZIP UnZip 6.xx             Info-ZIP Zip 3.0
+# Raspbian    Info-ZIP UnZip 6.00             Info-ZIP Zip 3.0
+# Windows
+#             GNU UnZip (from INFO-Zip UnZip) GNU Zip (from INFO-Zip Zip)
+#             7-Zip Command Line Version      7-Zip Command Line Version
+#             PKWare pkunzip                  PKWare pkzip
+#             WinZip CLI                      WinZip CLI
+#             WinRAR CLI                      WinRAR CLI
+#
+# First, check for the programs that would be installed by default.
+# If none can be found, raise an error and tell the user how they can resolve
+# the problem: Either tell them to install a different backend gem (prefered)
+# or tell them how to install a supported CLI program (depending on OS).
+#
+# When using this shell method, always emit a warning that it is very
+# inefficient and should not never no-way no-how no-where be used in
+# production environments.
+
+module MultiZip::Backend::Cli
+  def self.extended(mod)
+    if strategy = strategy
+      require "multi_zip/backend/shell/#{strategy.require_name}"
+      extend strategy.extend_name
+      warn([
+        "MultiZip is using the \"#{strategy.human_name}\" command-line program.",
+        "This is very inefficient and should not be used in production environments."
+      ].join(' '))
+    else
+      raise NoSupportedBackendError, "MultiZip::Backend::Cli could find no suitable zipping/unzipping programs in path."
+    end
+  end
+
+  def self.strategy_available?
+    !!strategy
+  end
+
+  def self.strategy
+    @strategy ||= detect_strategy
+  end
+
+  def self.detect_strategy
+    nil # xxx
+  end
+end
